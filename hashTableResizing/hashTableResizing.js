dea94bd6b1fb4e97c8662cf3aab205b67898200f
nickrotondo <nickrotondo@gmail.com>
2020-08-27 20:19:13 -0700

    Create hash table with insert, retrieve and remove methods

--- hashTableResizing/hashTableResizing.js
+++ hashTableResizing/hashTableResizing.js
@@ -22,23 +22,94 @@ var getIndexBelowMaxForKey = function(str, max) {
 
 var makeHashTable = function() {
   var result = {};
-  var storage = [];
-  var storageLimit = 4;
-  var size = 0;
-  
-  result.insert = function(/*...*/ 
-) {
-    // TODO: implement `insert`
+  result.storage = [];
+  result.storageLimit = 4;
+  result.size = 0;
+
+  result.insert = function( key, value ) {
+    var storageIndex = getIndexBelowMaxForKey(key, result.storageLimit);
+    var bucket = result.storage[storageIndex]
+
+    // if bucket at storageIndex doesn't exist
+    if ( !bucket ) {
+      // create new bucket
+      result.storage[storageIndex] = [];
+      bucket = result.storage[storageIndex];
+    }
+
+    var found = false;
+    // iterate over bucket
+    for (var i = 0; i < bucket.length; i++) {
+      var tuple = bucket[i];
+      // if the key exists in the bucket
+      if (tuple[0] === key) {
+        // update the value
+        result.storage[index][1] = value;
+        found = true;
+      }
+    }
+
+    // if no key was found
+    if (!found) {
+      // insert new key and value as tuple in bucket
+      result.storage[storageIndex].push([key, value]);
+    }
+
+    // increment size
+    result.size++;
+
   };
 
-  result.retrieve = function(/*...*/ 
-) {
-    // TODO: implement `retrieve`
+  result.retrieve = function( key ) {
+    var storageIndex = getIndexBelowMaxForKey(key, this.storageLimit);
+    var bucket = this.storage[storageIndex]
+
+    // if bucket at storageIndex doesn't exist
+    if (bucket === undefined) {
+      // return undefined
+      return undefined;
+    }
+
+    // iterate over bucket
+    for (var i = 0; i < bucket.length; i++) {
+      var tuple = bucket[i];
+      // if the key exists in the bucket
+      if (tuple[0] === key) {
+        // update the value
+        return tuple[1];
+      }
+    }
+
+    // if no key was found, return undefined
+    return undefined;
   };
 
-  result.remove = function(/*...*/ 
-) {
-    // TODO: implement `remove`
+  result.remove = function( key ) {
+    // call hashing function on key with storageLimit to get a storageIndex
+    var storageIndex = getIndexBelowMaxForKey(key, result.storageLimit);
+    var bucket = result.storage[storageIndex]
+
+    // if only one tuple in bucket and the key matches the tuple key
+    if (bucket.length === 1 && bucket[0][0] === key) {
+        // delete the bucket
+        delete result.storage[storageIndex];
+    } else {
+      // iterate over bucket
+      for (var i = 0; i < bucket.length; i++) {
+        var tuple = bucket[i];
+        // if the key exists in the bucket
+        if (tuple[0] === key) {
+          // delete the tuple
+          result.storage[storageIndex].splice(i, 1);
+          return tuple[1];
+          // delete result.storage[storageIndex][0];
+          // delete result.storage[storageIndex][1];
+        }
+      }
+    }
+
+    // decrement storage size
+    result.size--;
   };
 
   return result;
