02f893ce24d08f78428ed10ec7ad0b4d661814f6
nickrotondo <nickrotondo@gmail.com>
2020-09-15 19:29:45 -0700

    Complete function binarySearch

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -11,5 +11,27 @@
  */
 
 var binarySearch = function (array, target) {
+
+  var result = null;
+
+  var findIt = (array, start, end, target) => {
+    var searchIndex = Math.floor((end + start) / 2);
+    if (start === end) {
+      return;
+    }
+    if (array[searchIndex] === target) {
+      result = searchIndex;
+      return;
+    } else if (array[searchIndex] > target) {
+      findIt(array, start, searchIndex - 1, target);
+    } else {
+      findIt(array, searchIndex + 1, end, target);
+    }
+  }
+
+  findIt(array, 0, array.length - 1, target);
+
+  return result;
+
 };
 
