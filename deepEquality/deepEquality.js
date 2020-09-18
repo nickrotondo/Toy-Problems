3facc7d50569e051d038bb1fa12498fa0f627d28
nickrotondo <nickrotondo@gmail.com>
2020-09-17 19:17:19 -0700

    Complete function deepEquals

--- deepEquality/deepEquality.js
+++ deepEquality/deepEquality.js
@@ -11,5 +11,53 @@
   * don't worry about handling cyclical object structures.
   *
   */
-var deepEquals = function(apple, orange) {
+var deepEquals = function (apple, orange) {
+  if (apple === orange) {
+    return true;
+  } else if ((typeof (apple) === 'object' && apple !== null) && (typeof (orange) === 'object' && orange !== null)) {
+    if (Object.keys(apple).length !== Object.keys(orange).length) {
+      return false
+    }
+
+    for (var key in apple) {
+      if (orange.hasOwnProperty(key)) {
+        if (!deepEquals(apple[key], orange[key])) {
+          return false;
+        }
+      } else {
+        return false;
+      }
+    }
+
+    return true;
+
+  } else {
+    return false;
+  }
 };
+
+// Check if apple = orange
+// If apple and orange are both objects
+  // Check if the lengths of their keys match
+  // Iterate over apple's keys
+    // Check if they both have the same key values
+    // Check if apple[key] = orange[key]
+
+// —————————————————
+
+// if apple === orange
+  // return true
+
+// else if the value at apple is an object and value of orange is an object
+  // if the length of apple doesn't equal the length of orange
+    // return false
+  // iterate over keys in apple
+    // if orange has the same keys
+      // if calling deepEquals on apple[key] and orange[key] is not true
+        // return false
+    // otherwise
+      // return false
+  // return true
+
+// otherwise
+  // return false
