e7b4b616b4770327a865394bff2da675a1149de1
nickrotondo <nickrotondo@gmail.com>
2020-09-25 19:15:30 -0700

    Complete commonCharacters function with two input strings

--- commonCharacters/commonCharacters.js
+++ commonCharacters/commonCharacters.js
@@ -10,8 +10,29 @@
  * Extra credit: Extend your function to handle more than two input strings.
  */
 
-
+// Create results array
+// Iterate over string1
+  // If current ccharacter = ' '
+    // skip
+  // Iterate over string2
+    // If current character in string1 = current character in string2
+      // add to results array and continue to next character in string1
+// return results array as a string
 
 var commonCharacters = function(string1, string2) {
-  // TODO: Your code here!
+  let results = [];
+
+  for (let i = 0; i < string1.length; i++) {
+    if (string1[i] === ' ') {
+      continue;
+    }
+    for (let j = 0; j < string2.length; j++) {
+      if (string1[i] === string2[j] && results.indexOf(string1[i]) === -1) {
+        results.push(string1[i]);
+        break;
+      }
+    }
+  }
+
+  return results.join('');
 };
