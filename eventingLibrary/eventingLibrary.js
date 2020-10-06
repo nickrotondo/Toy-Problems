67e4d50dc105535c43a5b7205613050108a2839d
nickrotondo <nickrotondo@gmail.com>
2020-10-05 19:11:15 -0700

    Complete eventing library

--- eventingLibrary/eventingLibrary.js
+++ eventingLibrary/eventingLibrary.js
@@ -20,7 +20,24 @@
  * - It is not necessary to write a way to remove listeners.
  */
 
-var mixEvents = function(obj) {
-  // TODO: Your code here
+var mixEvents = function (obj) {
+  var events = {};
+
+  obj.trigger = function (event) {
+    var args = Array.prototype.slice.call(arguments, 1);
+    if (events[event]) {
+      for (var i = 0; i < events[event].length; i++) {
+        events[event][i].apply(this, args);
+      }
+    }
+  }
+
+  obj.on = function (event, callback) {
+    if (!events[event]) {
+      events[event] = [];
+    }
+    events[event].push(callback);
+  };
+
   return obj;
 };
