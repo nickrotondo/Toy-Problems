0e5b50b8c943fe7dac617139ee00420a61461b15
nickrotondo <nickrotondo@gmail.com>
2020-12-01 14:24:59 -0800

    Sudo code for palindrome checker function

--- longestPalindrome/longestPalindrome.js
+++ longestPalindrome/longestPalindrome.js
@@ -4,7 +4,21 @@
 * palindrome is "a racecar a". Count whitespaces as valid characters. Other
 * palindromes in the above string include "dad", "ete", " dad " (including
 * whitespace on each side of dad).
+
+* Leetcode #5
 */
 
-var longestPalindrome = function (string) {
+let longestPalindrome = function (string) {
+
 };
+
+// PALINDROME CHECKER FUNCTION:
+//   Set two counters - one to start at the first character and one at the last.
+//   If the characters don't match,
+//     return false
+//   If they do,
+//     go to the next letter in both directions
+//   Stop when they get to the middle letter
+
+// LONGEST PALINDROME SUBSTRING FINDER:
+//
