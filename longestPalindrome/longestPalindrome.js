c90ce32816ca56dd3effbad55afb6ff6cea5cf43
nickrotondo <nickrotondo@gmail.com>
2020-12-02 10:35:45 -0800

    Add palindrome checker helper function

--- longestPalindrome/longestPalindrome.js
+++ longestPalindrome/longestPalindrome.js
@@ -12,13 +12,27 @@ let longestPalindrome = function (string) {
 
 };
 
+let palindromeChecker = function (substring) {
+  let start = 0;
+  let end = substring.length - 1;
+  while (start < end) {
+    if (substring[start] !== substring[end]) {
+      return false;
+    } else {
+      start++;
+      end--;
+    }
+  }
+  return true;
+}
+
 // PALINDROME CHECKER FUNCTION:
 //   Set two counters - one to start at the first character and one at the last.
 //   If the characters don't match,
 //     return false
 //   If they do,
 //     go to the next letter in both directions
-//   Stop when they get to the middle letter
+//   Stop when they get to the middle letter(s)
 
 // LONGEST PALINDROME SUBSTRING FINDER:
 //
