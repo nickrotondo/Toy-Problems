7b8085c1a37d340ece8437298e4694aa6954d985
nickrotondo <nickrotondo@gmail.com>
2020-12-02 10:59:12 -0800

    Complete longestPalindrome function

--- longestPalindrome/longestPalindrome.js
+++ longestPalindrome/longestPalindrome.js
@@ -9,21 +9,30 @@
 */
 
 let longestPalindrome = function (string) {
-
-};
-
-let palindromeChecker = function (substring) {
-  let start = 0;
-  let end = substring.length - 1;
-  while (start < end) {
-    if (substring[start] !== substring[end]) {
-      return false;
-    } else {
-      start++;
-      end--;
+  let currentLongest = '';
+  for (let i = 0; i < string.length; i++) {
+    for (let j = i + 1; j <= string.length; j++) {
+      let currentSubstring = string.substring(i, j)
+      if (palindromeChecker(currentSubstring) && currentSubstring.length > currentLongest.length) {
+        currentLongest = currentSubstring;
+      }
     }
   }
-  return true;
+  return currentLongest;
+};
+
+let palindromeChecker = function (sub) {
+  let start = 0;
+  let end = sub.length - 1;
+  let isPalindrome = true;
+  while (start <= end) {
+    if (sub[start] !== sub[end]) {
+      isPalindrome = false;
+    }
+    start++;
+    end--;
+  }
+  return isPalindrome;
 }
 
 // PALINDROME CHECKER FUNCTION:
@@ -35,4 +44,10 @@ let palindromeChecker = function (substring) {
 //   Stop when they get to the middle letter(s)
 
 // LONGEST PALINDROME SUBSTRING FINDER:
-//
+//   Set longest palindrome variable to empty string
+//   Iterate over the string
+//     for each character, iterate over the string again
+//       check each iteration of substring with palindromeChecker and if true AND
+//       it's longer than the current longest palindrome
+//         set current substring as longest palindrome
+//   return longest palindrome
