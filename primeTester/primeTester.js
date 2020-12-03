537542de6856e3c3cee3450fafa947595a929959
nickrotondo <nickrotondo@gmail.com>
2020-12-03 09:57:41 -0800

    Complete primeTester function with simple loop

--- primeTester/primeTester.js
+++ primeTester/primeTester.js
@@ -5,11 +5,16 @@
  */
 
 var primeTester = function(n) {
-  if (typeof n !== 'number' || n < 1 || n % 1 !== 0) {
+  if (typeof n !== 'number' || n < 2 || n % 1 !== 0) {
     // n isn't a number or n is less than 1 or n is not an integer
     return false;
   }
-  // TODO: return true if n is prime, false otherwise
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
 };
 
 /* Extra credit: Write a function that generates a list of all prime numbers
