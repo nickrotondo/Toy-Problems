51c20671f3565d04abce24b2d3f8d9e474cd4118
nickrotondo <nickrotondo@gmail.com>
2020-12-07 10:41:14 -0800

    Complete threeSum using two pointers

--- threeSum.js
+++ threeSum.js
@@ -6,14 +6,17 @@ Notice that the solution set must not contain duplicate triplets.
 
 Example 1:
   Input: nums = [-1,0,1,2,-1,-4]
+    threeSum([-1,0,1,2,-1,-4])
   Output: [[-1,-1,2],[-1,0,1]]
 
 Example 2:
   Input: nums = []
+    threeSum([])
   Output: []
 
 Example 3:
   Input: nums = [0]
+    threeSum([0])
   Output: []
 
 Constraints:
@@ -22,5 +25,25 @@ Constraints:
 */
 
 var threeSum = function(nums) {
-
+  nums.sort((a, b) => a - b);
+  let result = [];
+  for (let i = 0; i < nums.length; i++) {
+    let target = -nums[i];
+    let left = i + 1, right = nums.length - 1;
+    while (left < right) {
+      if (nums[left] + nums[right] == target) {
+        result.push([nums[i], nums[left], nums[right]]);
+        while (nums[left + 1] == nums[left++]);
+        while (nums[right - 1] == nums[right--]);
+      } else if (nums[left] + nums[right] < target) {
+          left++;
+      } else {
+        right--;
+      }
+    }
+    while (nums[i + 1] == nums[i]) {
+      i++
+    }
+  }
+  return result;
 };
