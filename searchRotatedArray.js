14ad7f07b5e4008675473d3dad9067a165a7cd2b
nickrotondo <nickrotondo@gmail.com>
2020-12-16 17:22:50 -0800

    Complete search in rotated sorted array

--- searchRotatedArray.js
+++ searchRotatedArray.js
@@ -29,5 +29,39 @@ Constraints:
 */
 
 var search = function(nums, target) {
+  if (nums == null || nums.length === 0) return -1;
+  let left = 0, right = nums.length - 1;
+  while (left < right) {
+    let midpoint = Math.floor(left + (right - left) / 2);
+    if (nums[midpoint] > nums[right]) {
+      left = midpoint + 1;
+    } else {
+      right = midpoint;
+    }
+  }
 
+  [4,5,6,7,0,1,2]
+
+  let pivotPoint = left;
+  left = 0;
+  right = nums.length - 1;
+
+  if (target >= nums[pivotPoint] && target <= nums[right]) {
+    left = pivotPoint;
+  } else {
+    right = pivotPoint - 1;
+  }
+
+  while (left <= right) {
+    let midpoint = Math.floor(left + (right - left) / 2);
+    if (nums[midpoint] === target) {
+      return midpoint;
+    } else if (nums[midpoint] < target) {
+      left = midpoint + 1;
+    } else {
+      right = midpoint - 1;
+    }
+  }
+
+  return -1;
 };
